5ee44b4c441df836754662e218df81f436f0b5af
Khusbu <khusbu.bhushal@go.stcloudstate.edu>
2025-12-28 20:11:35 -0600

    Note method signatures

--- Note.java
+++ Note.java
@@ -1,8 +1,32 @@
+import java.util.*;
 
 public class Note{
-public static void main(String[] args){
+    private String title;
+    private String body;
 
-}
+
+    //Constructor 
+    public Note(String title, String body){
+
+    }
+    
+    //Getters
+    public String getTitle(){
+        return title;
+    }
+
+    public String getBody(){
+        return body;
+    }
+
+    //Setters 
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public void setBody(String body){
+        this.body = body;
+    }
 }
 
 
--- NoteManager.java
+++ NoteManager.java
@@ -1,3 +1,14 @@
 public class NoteManager {
     
-}
+
+    public final class Files extends Object
+
+    public boolean createNote(){
+
+    }
+
+    public boolean deleteNote(){
+
+    }
+
+    public boolean modifyTitle
